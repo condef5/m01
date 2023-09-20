8865fcc7da0a0237033b9f0a8482ea50486b4684
Frank Condezo <frankcondezo@gmail.com>
2023-09-20 17:54:09 -0500

    add parent and sub_section relations

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,6 @@
 class Section < ApplicationRecord
+  validates :title, presence: true
+
+  belongs_to :parent, class_name: 'Section', optional: true
+  has_many :sub_sections, class_name: 'Section', foreign_key: 'parent_id'
 end
