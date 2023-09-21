0f9d5387298b9ed707ff761ba1754a9b6a3efa89
Frank Condezo <frankcondezo@gmail.com>
2023-09-20 19:25:46 -0500

    feat: add sections (#1)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        // sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Arial', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        theme: {
+          gray: "#6c757d", // Define the "gris" color with the desired value
+        },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
