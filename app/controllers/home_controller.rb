06748360408c6d996f4eaee0c3c0ceb628562de5
Frank Condezo <frankcondezo@gmail.com>
2023-09-20 19:21:48 -0500

    add recursive sections to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @sections = Section.where(parent_id: nil).order(:title).includes(:sub_sections)
+    @sections = Section.where(parent_id: nil).order(:created_at).includes(:sub_sections)
   end
 end
